0e3f9abb77df94d7a079ea7fcbab45a519550d23
amithkumarkulkarni <azureamith@gmail.com>
2026-06-16 13:01:23 +0530

    Update prod for release

--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,3 +1,4 @@
 aws_region  = "ap-south-1"
-bucket_name = "prod-7453-s3-bucket"
 environment = "prod"
+bucket_name = "prod-7453-s3-bucket"
+vpc_name    = "prod-vpc"
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,11 +1,15 @@
-variable "bucket_name" {
-  type = string
-}
-
 variable "environment" {
   type = string
 }
 
 variable "aws_region" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "vpc_name" {
   type = string
 }
