bb6207ce8bbb55cf73f4669c63acfa50ea363e55
amithkumarkulkarni <azureamith@gmail.com>
2026-06-17 19:46:31 +0530

    Updated VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,11 +8,46 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
   count = length(var.private_subnet_cidrs)
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = var.azs[count.index]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
 
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index + 1}"
+
+    "kubernetes.io/cluster/dev-aks-cluster" = "shared"
+    "kubernetes.io/role/elb"                = "1"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
